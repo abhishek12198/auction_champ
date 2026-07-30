76cb656aa1d55e72765e98b9c1806dd7c27b5d00
abhishek <amiabhi2401@gmail.com>
2026-07-30 23:02:06 +0530

    aUCTION CHAMP CHANGES

--- auction_module/__manifest__.py
+++ auction_module/__manifest__.py
@@ -38,7 +38,7 @@
 
 {
     'name' : 'Auction Champ',
-    'version' : '1.5.278',
+    'version' : '1.5.284',
     'summary': 'Sports Auction module',
     'sequence': 10,
     'icon': '/auction_module/static/description/icon.png',
--- auction_module/static/src/js/sold_sound.js
+++ auction_module/static/src/js/sold_sound.js
@@ -1,17 +1,31 @@
 odoo.define('auction_module.sold_sound', function (require) {
     'use strict';
 
-    var rpc = require('web.rpc');
-    var Dialog = require('web.Dialog');
+    var AUD = '/auction_module/static/src/audio/';
+    var VER = '?v=282m';
 
-    // Preload audio
-    const soldAudio = new Audio('/auction_module/static/src/audio/congrats.mp3');
+    function playClip(src) {
+        try {
+            var a = new Audio(src + VER);
+            a.volume = 1;
+            var p = a.play();
+            if (p && p.catch) {
+                p.catch(function (e) {
+                    console.warn('Audio play failed:', e);
+                });
+            }
+            return a;
+        } catch (e) {
+            console.warn('Audio play failed:', e);
+            return null;
+        }
+    }
 
     function playSoldSound() {
-        soldAudio.currentTime = 0;  // restart sound
-        soldAudio.play().catch(function (e) {
-            console.warn('Audio play failed:', e);
-        });
+        playClip(AUD + 'sold_voice.mp3');
+        setTimeout(function () {
+            playClip(AUD + 'sold_clap.wav');
+        }, 420);
     }
 
     // Expose globally so it can be called from buttons
