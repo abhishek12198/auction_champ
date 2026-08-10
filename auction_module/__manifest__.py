e1e891fc8f50e4f8e8f9f8a1f45f6eafb8736f88
abhishek <amiabhi2401@gmail.com>
2026-08-10 20:33:07 +0530

    [ADD] - Reg link for amdin

--- auction_module/__manifest__.py
+++ auction_module/__manifest__.py
@@ -38,7 +38,7 @@
 
 {
     'name' : 'Auction Champ',
-    'version' : '1.5.456',
+    'version' : '1.5.469',
     'summary': 'Sports Auction module',
     'sequence': 10,
     'icon': '/auction_module/static/description/icon.png',
