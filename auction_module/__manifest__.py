a5c88a97d9e2fca8a51ceb6caa783e5306e37f36
abhishek <amiabhi2401@gmail.com>
2026-08-27 09:55:37 +0530

    [ADD] - Changes

--- auction_module/__manifest__.py
+++ auction_module/__manifest__.py
@@ -38,7 +38,7 @@
 
 {
     'name' : 'Auction Champ',
-    'version' : '1.5.627',
+    'version' : '1.5.632',
     'summary': 'Sports Auction module',
     'sequence': 10,
     'icon': '/auction_module/static/description/icon.png',
